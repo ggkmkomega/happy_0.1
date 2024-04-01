6a7b570fac316020815dc08d66e25b5e6c14115f
ggkmkomega <dhuibetilyes@gmail.com>
2024-04-01 12:28:43 +0000

    single listing grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { unstable_noStore as noStore } from "next/cache";
 import SearchFilters from "./components/SearchFilters";
 import Map from "./components/Map";
+import SingleListing from "./components/SingleListing";
 
 export default async function Home() {
   noStore();
@@ -9,27 +10,15 @@ export default async function Home() {
     <>
       <SearchFilters />
       <div className="absolute ">
+        <div className="grid grid-cols-2 ">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <div className="flex" key={index}>
+              <SingleListing />
+            </div>
+          ))}
+        </div>
         <Map />
       </div>
     </>
-    // <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-    //   <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-    //     <div className="flex flex-col items-center gap-2">
-    //       <div className="flex flex-col items-center justify-center gap-4">
-    //         <p className="text-center text-2xl text-white">
-    //           {session && <span>Logged in as {session.user?.name}</span>}
-    //         </p>
-    //         <Link
-    //           href={session ? "/api/auth/signout" : "/api/auth/signin"}
-    //           className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-    //         >
-    //           {session ? "Sign out" : "Sign in"}
-    //         </Link>
-    //       </div>
-    //     </div>
-    //     <Listings />
-    //     <Suggest />
-    //   </div>
-    // </main>
   );
 }
