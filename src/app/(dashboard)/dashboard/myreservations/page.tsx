335e824794eddb7ef5d1448ed3766123a114d4b9
ggkmkomega <dhuibetilyes@gmail.com>
2024-05-31 21:28:54 +0100

    No reservation empty state

--- src/app/(dashboard)/dashboard/myreservations/page.tsx
+++ src/app/(dashboard)/dashboard/myreservations/page.tsx
@@ -17,10 +17,14 @@ import { DashboardHeader } from "~/_components/header";
 
 import MyReservationTableRow from "~/app/_components/MyReservationTableRow";
 import { api } from "~/trpc/react";
+import { EmptyPlaceholder } from "~/_components/empty-placeholder";
+import { Button } from "~/_components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Reservations() {
   const { data: userReservations } =
     api.reservation.getAllUserReservations.useQuery();
+  const router = useRouter();
   return (
     <DashboardShell>
       <DashboardHeader
@@ -28,49 +32,69 @@ export default function Reservations() {
         text="Browse Your reservations."
       />
       <div className="flex w-full flex-col">
-        <main className="flex flex-1 flex-col gap-4  md:gap-8 ">
-          <Card>
-            <CardHeader></CardHeader>
-            <CardContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="hidden w-[100px] sm:table-cell">
-                      <span className="sr-only">Image</span>
-                    </TableHead>
-                    <TableHead>Listing</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead className="hidden md:table-cell">
-                      Price
-                    </TableHead>
-                    <TableHead className="hidden md:table-cell">
-                      Starts at
-                    </TableHead>
-                    <TableHead className="hidden md:table-cell">
-                      Ends at
-                    </TableHead>
-                    <TableHead>
-                      <span className="sr-only">Actions</span>
-                    </TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {userReservations?.map((reservation) => (
-                    <MyReservationTableRow
-                      key={reservation.id}
-                      Reservation={reservation}
-                    />
-                  ))}
-                </TableBody>
-              </Table>
-            </CardContent>
-            <CardFooter>
-              <div className="text-xs text-muted-foreground">
-                Showing <strong>1-10</strong> of <strong>32</strong> products
-              </div>
-            </CardFooter>
-          </Card>
-        </main>
+        {userReservations?.length ? (
+          <main className="flex flex-1 flex-col gap-4  md:gap-8 ">
+            <Card>
+              <CardHeader></CardHeader>
+              <CardContent>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="hidden w-[100px] sm:table-cell">
+                        <span className="sr-only">Image</span>
+                      </TableHead>
+                      <TableHead>Listing</TableHead>
+                      <TableHead>Status</TableHead>
+                      <TableHead className="hidden md:table-cell">
+                        Price
+                      </TableHead>
+                      <TableHead className="hidden md:table-cell">
+                        Starts at
+                      </TableHead>
+                      <TableHead className="hidden md:table-cell">
+                        Ends at
+                      </TableHead>
+                      <TableHead>
+                        <span className="sr-only">Actions</span>
+                      </TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {userReservations?.map((reservation) => (
+                      <MyReservationTableRow
+                        key={reservation.id}
+                        Reservation={reservation}
+                      />
+                    ))}
+                  </TableBody>
+                </Table>
+              </CardContent>
+              <CardFooter>
+                <div className="text-xs text-muted-foreground">
+                  Showing <strong>1-10</strong> of <strong>32</strong>{" "}
+                  Reservation
+                </div>
+              </CardFooter>
+            </Card>
+          </main>
+        ) : (
+          <EmptyPlaceholder>
+            <EmptyPlaceholder.Icon name="post" />
+            <EmptyPlaceholder.Title>No Reservation </EmptyPlaceholder.Title>
+            <EmptyPlaceholder.Description>
+              You don&apos;t have any Reservations yet. Start your journey.
+            </EmptyPlaceholder.Description>
+            <Button
+              onClick={() => {
+                router.push("/");
+              }}
+              variant="default"
+              size="sm"
+            >
+              Browse Listings
+            </Button>
+          </EmptyPlaceholder>
+        )}
       </div>
     </DashboardShell>
   );
